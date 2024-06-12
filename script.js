be37b4b77a130459d2f64cec76b94f707aee01f4
Reaksmey1233 <Derclash134@gmail.com>
2024-06-12 21:20:55 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const yesButton = document.querySelector(".yes");
+const noButton = document.querySelector(".no");
+const text = document.querySelector("h2");
+
+noButton.addEventListener("mouseover", function () {
+  function getRandomNumber() {
+    return Math.floor(Math.random() * 301);
+  }
+
+  noButton.style.left = getRandomNumber() + "px";
+  noButton.style.top = getRandomNumber() + "px";
+});
+
+yesButton.onclick = () => (text.innerText = "YES I DO🩷");
